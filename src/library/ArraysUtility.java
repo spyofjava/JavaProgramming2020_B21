2c7aafb1816376f1c1dbaead0808bbc041edecb5
Ibrahim <spyofjava@gmail.com>
2020-11-16 22:39:01 -0800

    Nov_16

--- src/library/ArraysUtility.java
+++ src/library/ArraysUtility.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 
 public class ArraysUtility {
 
-    public static int max(int[] arr){
-        int max =arr[0];
-        for( int each  : arr ){
-            if(each > max){
+    public static int max(int[] arr) {
+        int max = arr[0];
+        for (int each : arr) {
+            if (each > max) {
                 max = each;
             }
         }
         return max;
     }
 
-    public static int min(int[] arr){
+    public static int min(int[] arr) {
         int min = arr[0];
 
-        for(int each : arr) {
+        for (int each : arr) {
             if (each < min) {
                 min = each;
             }
@@ -26,33 +26,33 @@ public class ArraysUtility {
         return min;
     }
 
-    public static int[] combineTwoArrays(int[] arr1, int[] arr2){
-        int[] arr3 = new int[arr1.length+arr2.length];
+    public static int[] combineTwoArrays(int[] arr1, int[] arr2) {
+        int[] arr3 = new int[arr1.length + arr2.length];
         int i = 0;
 
-        for(int each : arr1){
+        for (int each : arr1) {
             arr3[i++] = each;
         }
 
-        for(int each : arr2){
-            arr3[i++]  = each;
+        for (int each : arr2) {
+            arr3[i++] = each;
         }
 
         return arr3;
     }
 
-    public static int[] sort(int[] arr){  // {2,1,3}
+    public static int[] sort(int[] arr) {  // {2,1,3}
         Arrays.sort(arr);  // {1,2,3};
         int[] reversedArray = new int[arr.length];  // {3, 2, 1}
 
-        for(int i=arr.length-1, j = 0 ;  i>=0; i--, j++){
+        for (int i = arr.length - 1, j = 0; i >= 0; i--, j++) {
             reversedArray[j] = arr[i];
         }
 
         return reversedArray;
     }   // sorts in descending order
 
-    public static String printArray(int[] arr){
+    public static String printArray(int[] arr) {
         String str = Arrays.toString(arr);
         str = str.replace("[", "{").replace("]", "}");
         return str;
