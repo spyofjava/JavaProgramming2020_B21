9a36f670007ea87d521b639569d2b4f8c23ed11f
Ibrahim <spyofjava@gmail.com>
2020-11-17 22:50:46 -0800

    Nov_17

--- src/library/exercise_Nov17.java
+++ src/library/exercise_Nov17.java
@@ -1,4 +1,32 @@
 package library;
 
+import java.util.Arrays;
+
 public class exercise_Nov17 {
+    public static void main(String[] args) {
+
+        String[] arr = {"\"zero\"", "\"one\"", "\"two\"", "\"three\"", "\"four\"", "\"elkj\""};
+
+        System.out.println(Arrays.toString(getWithE(arr)));
+
+
+
+    }
+    public static String[] getWithE(String[] arr) {
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].contains("e")) count++;
+        }
+
+        String[] arr1 = new String[count];
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].contains("e")) arr1[j++] = arr[i];
+        }
+
+        return arr1;
+    }
+
+
 }
