8e0c0ca21de55fe25de26d89cf623dfbb46260a0
Ibrahim <spyofjava@gmail.com>
2020-11-18 16:27:58 -0800

    Nov_18

--- src/day20_ForLoop/Timer.java
+++ src/day20_ForLoop/Timer.java
@@ -11,16 +11,12 @@ public class Timer {
         int minutes=scan.nextInt();
         for (int i=minutes; i > 0 ;--i) {
             if( i< 0 ) {
-
                 break;
-
             }
-
             for (int z=59; z >=0 ; --z) {
                 System.out.print("\r"+ (i-1)+" minutes and "+z+" seconds left");
                 Thread.sleep(2000);
             }
-
         }
 
         System.out.println("\n\n");
