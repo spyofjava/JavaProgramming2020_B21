90b323f603214c69c4fded53e8f9fa4ae72ffcb9
Ibrahim <spyofjava@gmail.com>
2020-12-01 08:53:50 -0800

    Nov_30

---
+++ src/day34_ArrayList/bos.java
@@ -0,0 +1,10 @@
+package day34_ArrayList;
+
+public class bos {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ src/day35_CustomClass/ArrayListPractice1.java
@@ -0,0 +1,48 @@
+package day35_CustomClass;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListPractice1 {
+    public static void main(String[] args) {
+        String[] countryNames = {"Andorra","United Arab Emirates","Afghanistan","Turkey","United States"," Antigua and Barbuda","Albania", "Colombia", "Venezuela", "Panama","United Kingdom", "Mexico ", "United States"};
+
+        ArrayList<String> countries = new ArrayList<>(  Arrays.asList(countryNames)  );
+        System.out.println(countries);
+
+        //    Predicate<String> length10 = p ->  p.length() >= 10;
+
+        countries.removeIf( p ->  p.length() >= 10 );
+
+        System.out.println(countries);
+
+        System.out.println("====================================================");
+        LocalDate[] arr1 = {
+                LocalDate.of(2010, 11, 23),
+                LocalDate.of(2012, 11, 23),
+                LocalDate.of(2013, 11, 23),
+                LocalDate.of(2014, 11, 23),
+                LocalDate.of(2015, 11, 23),
+                LocalDate.of(2016, 7, 23),
+                LocalDate.of(2017, 11, 23),
+                LocalDate.of(2018, 11, 23),
+                LocalDate.of(2019, 11, 23),
+                LocalDate.of(2020, 11, 23),
+        };
+
+        LocalDate d1 = LocalDate.of(2016, 8, 15);
+        ArrayList<LocalDate> dates = new ArrayList<>( Arrays.asList(arr1) );
+        dates.removeIf( p ->  p.isBefore(d1) );
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("MMM-dd-yyyy, EEE");
+
+        for(LocalDate each : dates){
+            System.out.println(each.format(df));
+        }
+
+
+
+    }
+}
