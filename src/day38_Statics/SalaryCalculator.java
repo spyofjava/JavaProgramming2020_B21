aab215c04bf68f16a959b2cf96e835e032152fa8
Ibrahim <spyofjava@gmail.com>
2020-12-07 08:55:31 -0800

    Dec_7

--- src/day38_Statics/SalaryCalculator.java
+++ src/day38_Statics/SalaryCalculator.java
@@ -1,45 +1,49 @@
 package day38_Statics;
 
 public class SalaryCalculator {
-    public  double hourlyRate, weeklyHours, stateTaxRate, federalTaxRate, salary, salaryAfterTax;
+    public double hourlyRate, weeklyHours, stateTaxRate, federalTaxRate, salary, salaryAfterTax;
 
-    public void setInfo(double hourlyRate,double weeklyHours,double stateTaxRate ,double federalTaxRate){
+    public void setInfo(double hourlyRate, double weeklyHours, double stateTaxRate, double federalTaxRate ){
         this.hourlyRate = hourlyRate;
         this.weeklyHours = weeklyHours;
-        this.stateTaxRate = (stateTaxRate >1)? stateTaxRate/100 :stateTaxRate;
-        this.federalTaxRate = (federalTaxRate > 1)? federalTaxRate/100 :federalTaxRate;
-        this.salary = calculateSalaryAfterTax();
-        this.salaryAfterTax = calculateSalaryAfterTax();
+        this.stateTaxRate = (stateTaxRate > 1)? stateTaxRate / 100 : stateTaxRate;  // 8 will be 0.08
+        this.federalTaxRate = (federalTaxRate > 1) ? federalTaxRate/ 100 : federalTaxRate;
+        salary = calculateSalary();
+        salaryAfterTax = calculateSalaryAfterTax();
     }
 
-
     public double calculateSalary(){
-        return  hourlyRate*weeklyHours*52;
+        return hourlyRate * weeklyHours * 52;
     }
 
     public double calculateStateTax(){
-        return salary*stateTaxRate;
+        return salary * stateTaxRate;
     }
 
     public double calculateFederalTax(){
-        return salary*federalTaxRate;
+        return salary * federalTaxRate;
     }
 
-    public  double calculateTotalTax(){
-        return calculateFederalTax()+calculateStateTax();
+    public double calculateTotalTax(){
+        return calculateStateTax() + calculateFederalTax();
     }
 
-    public double  calculateSalaryAfterTax(){
-        return salary-calculateTotalTax();
+    public double calculateSalaryAfterTax(){
+        return salary - calculateTotalTax();
     }
 
 
-    @Override
     public String toString() {
         return "SalaryCalculator{" +
-                "hourlyRate=" + hourlyRate +
-                ", salary=" + salary +
-                ", salaryAfterTax=" + salaryAfterTax +
+                "hourlyRate= $" + hourlyRate +
+                ", salary= $" + salary +
+                ", salaryAfterTax= $" + salaryAfterTax +
                 '}';
     }
+
+    public boolean equals(SalaryCalculator s1){
+        return    this.salary == s1.salary;
+    }
+
+
 }
