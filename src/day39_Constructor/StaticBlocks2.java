7ce065a7738fa15101dd63215302b74f27e14093
Ibrahim <spyofjava@gmail.com>
2020-12-10 08:53:56 -0800

    Dec_10

---
+++ src/day39_Constructor/StaticBlocks2.java
@@ -0,0 +1,22 @@
+package day39_Constructor;
+
+public class StaticBlocks2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main Method");
+
+    }
+
+    static{
+        System.out.println("Static Block 1");
+    }
+
+    static{
+        System.out.println("Static block 2");
+    }
+
+    static{
+        System.out.println("Static block 3");
+    }
+}
