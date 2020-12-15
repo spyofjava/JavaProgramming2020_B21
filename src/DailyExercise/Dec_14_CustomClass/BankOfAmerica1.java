cf89ab9433fd61230e103c5c84d593bc8bfa050c
Ibrahim <spyofjava@gmail.com>
2020-12-14 19:37:26 -0800

    Dec_14

--- src/DailyExercise/Dec_14_CustomClass/BankOfAmerica1.java
+++ src/DailyExercise/Dec_14_CustomClass/BankOfAmerica1.java
@@ -1,4 +1,17 @@
 package DailyExercise.Dec_14_CustomClass;
 
 public class BankOfAmerica1 {
+
+    public static void main(String[] args) {
+
+        Empoyee1 emp1=new Empoyee1();
+
+        emp1.setInfo("Ayse", "Bank Of America", "QA", "ABC123", "Female", 45, 55);
+        System.out.println(emp1);
+
+
+        System.out.println("Total Tax: " +emp1.calculateTax(0.05,0.22));
+        System.out.println("Salary After Tax: " +(emp1.calculateSalary()-emp1.calculateTax(0.05,0.22)));
+        System.out.println();
+    }
 }
--- src/DailyExercise/Dec_14_CustomClass/Empoyee1.java
+++ src/DailyExercise/Dec_14_CustomClass/Empoyee1.java
@@ -3,4 +3,44 @@ package DailyExercise.Dec_14_CustomClass;
 public class Empoyee1 {
 
 
+    public String name, company, jobTitle, ID, gender;
+    public  int weeklyHours;
+    public double hourlyRate, salary;
+
+
+
+    public void setInfo(String name, String company, String jobTitle, String ID,String gender,int weeklyHours, double hourlyRate){
+        this.name=name;
+        this.company=company;
+        this.jobTitle=jobTitle;
+        this.ID=ID;
+        this.gender=gender;
+        this.weeklyHours=weeklyHours;
+        this.hourlyRate=hourlyRate;
+        this.salary=calculateSalary();
+
+    }
+
+    public double calculateSalary(){
+        salary=weeklyHours*hourlyRate*52;
+        return  salary;
+    }
+
+    public double calculateTax(double stateTaxRate,double federalTaxRate){
+        double totalTax=calculateSalary()*(stateTaxRate+federalTaxRate);
+        return totalTax;
+    }
+
+    @Override
+    public String toString() {
+        return  "name= " + name +
+                "\ncompany= " + company +
+                "\njobTitle= " + jobTitle +
+                "\nID= " + ID +
+                "\ngender= " + gender +
+                "\nweeklyHours= " + weeklyHours +
+                "\nhourlyRate= " + hourlyRate +
+                "\nsalary= " + salary;
+
+    }
 }
