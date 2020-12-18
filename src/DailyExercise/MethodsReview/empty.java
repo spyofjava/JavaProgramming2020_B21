dd637e767657af0e357d36a3a5433e02432d9020
Ibrahim <spyofjava@gmail.com>
2020-12-17 19:54:35 -0800

    Dec_17

---
+++ src/DailyExercise/MethodsReview/empty.java
@@ -0,0 +1,16 @@
+package DailyExercise.MethodsReview;
+
+public class empty {
+
+    public static void main(String[] args) {
+        Hello5Times();
+    }
+
+
+
+    public static void Hello5Times(){
+        for (int i=0;i<5;i++){
+            System.out.println("hello!!!");
+        }
+    }
+}
