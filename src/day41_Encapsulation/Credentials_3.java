baffdc75b16b3b61b30e183279603ce702f97b33
Ibrahim <spyofjava@gmail.com>
2020-12-24 15:23:32 -0800

    Dec_24

--- src/day41_Encapsulation/Credentials_3.java
+++ src/day41_Encapsulation/Credentials_3.java
@@ -5,43 +5,34 @@ public class Credentials_3 {
     private String userName = "cybertek";
     private double passWord = 1234;
 
+
+
+
+
+
+
+
     public String getUserName() {
         return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     public double getPassWord() {
         return passWord;
     }
 
-    public void setPassWord(double passWord) {
-        this.passWord = passWord;
+    public void setUserName(String userName) {
+        /*
+        if(userName.length() < 8){
+            return;
+        }
+                we can also do security check in setter of Encapsulation
+         */
+        this.userName = userName;
     }
 
-//    public String getUserName() {
-//        return userName;
-//    }
-
-//    public double getPassWord() {
-//        return passWord;
-//    }
-
-//    public void setUserName(String userName) {
-//        /*
-//        if(userName.length() < 8){
-//            return;
-//        }
-//                we can also do security check in setter of Encapsulation
-//         */
-//        this.userName = userName;
-//    }
-
-//    public void setPassWord(double passWord){
-//        this.passWord = passWord;
-//    }
+    public void setPassWord(double passWord){
+        this.passWord = passWord;
+    }
 
 
 }
