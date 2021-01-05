ce0b8b340dfbb42794c505834de2bfff81c234d4
Ibrahim <spyofjava@gmail.com>
2021-01-05 12:41:40 -0800

    Jan_5

--- src/day45_Exceptions/FinallyBlock.java
+++ src/day45_Exceptions/FinallyBlock.java
@@ -1,13 +1,15 @@
 package day45_Exceptions;
 
 public class FinallyBlock {
+
     public static void main(String[] args) {
 
-        try {
+        try{
             System.out.println(9/0);
         }catch (RuntimeException e){
             System.out.println("Catch Block");
-           // System.exit(0);
+            //  System.exit(0);
+
         }finally {
             System.out.println("Finally: Closed");
         }
@@ -15,5 +17,9 @@ public class FinallyBlock {
 
         System.out.println("Test Completed");
 
+
+
+
     }
+
 }
--- src/day45_Exceptions/ThrowsKeyword.java
+++ src/day45_Exceptions/ThrowsKeyword.java
@@ -6,18 +6,18 @@ import java.util.NoSuchElementException;
 
 public class ThrowsKeyword {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws InterruptedException {
 
-
-        /*try {
-            Thread.sleep(3000);
-        }catch (){
-            System.out.println();
-        }*/
 
         Thread.sleep(3000);
-        System.out.println("Cybertek".charAt(500));
+
+
+        //    System.out.println( "Cybertek".charAt(500) );
+
         System.out.println("Step 2");
+
+
+        Thread.sleep(3000);
 
 
 
