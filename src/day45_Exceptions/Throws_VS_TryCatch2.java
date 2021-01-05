b168720126332e22aaf4b3eba6ecdc324e554e2f
Ibrahim <spyofjava@gmail.com>
2021-01-05 13:30:33 -0800

    Jan_5

---
+++ src/day45_Exceptions/Throws_VS_TryCatch2.java
@@ -0,0 +1,35 @@
+package day45_Exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Throws_VS_TryCatch2 {
+
+    public static void method1() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+
+    public static void method2() throws InterruptedException{
+        method1();
+    }
+
+    public static void method3(){
+        try {
+            method2();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void Test(){
+        //   method1();
+        //    method2();
+
+        method3();
+
+    }
+
+
+
+}
