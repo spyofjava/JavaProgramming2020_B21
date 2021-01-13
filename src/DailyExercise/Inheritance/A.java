3c98f5752fd056039aadeb4818527f81839261b5
Ibrahim <spyofjava@gmail.com>
2021-01-12 17:46:43 -0800

    Jan_12

--- src/DailyExercise/Inheritance/A.java
+++ src/DailyExercise/Inheritance/A.java
@@ -1,58 +1,37 @@
 package DailyExercise.Inheritance;
 
 class A {
+    public static String name="Ibrahim";
+    public  String jobtitle;
+    public int age;
+
     public A() {
-        System.out.println("merhaba class A");
+
+        name="mehmet";
+        System.out.println("name: "+name);
+
     }
+
 
 }
 
 class B extends A{
+
+
     public B() {
         super();
-        //System.out.println("merhaba class B");
     }
 }
 
-class C extends B{
-    public C() {
-        super();
-        //System.out.println("merhaba class C");
-    }
-}
-
-class D extends C{
-    public D() {
-        super();
-        //System.out.println("merhaba class D");
-    }
-}
-
-
-class E {
+class C {
     public static void main(String[] args) {
-        System.out.println("-----------Step  I -----------------");
+
         A a=new A();
+        a.age=12;
+
+        System.out.print(a.age);
         System.out.println(a);
 
-
-        System.out.println("\n-----------Step II-----------------");
-        B b=new B();
-        System.out.println(b);
-
-
-        System.out.println("\n-----------Step III----------------");
-        C c=new C();
-        System.out.println(c);
-
-
-        System.out.println("\n-----------Step IV----------------");
-        D d=new D();
-        System.out.println(d);
-
-        System.out.println("\n-----------Step IV----------------");
-
-        E e=new E();
-        System.out.println(e);
+        //   ∫ß®åhîm
     }
 }
