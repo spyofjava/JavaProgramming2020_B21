4a97a452d1fe109c955ccb2f9c137c4ca1d815ca
Ibrahim <spyofjava@gmail.com>
2021-01-13 16:05:31 -0800

    Jan_13

--- src/DailyExercise/Empty.java
+++ src/DailyExercise/Empty.java
@@ -2,44 +2,7 @@ package DailyExercise;
 
 public class Empty {
 
-    public static void main(String[] args) throws InterruptedException {
 
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 4; j++) {
-                        System.out.print("*");
-                        if (i == 0 && j == 3) {
-                            System.out.print("*       *");
-                            for (int k = 0; k < 4; k++) {
-                                System.out.print("*");
-                            }
-                        } else if (i == 1 && j == 3) {
-                            System.out.print(" *     * ");
-                            for (int k = 0; k < 4; k++) {
-                                System.out.print("*");
-                            }
-                        } else if (i == 2 && j == 3) {
-                            System.out.print("  *   *  ");
-                            for (int k = 0; k < 4; k++) {
-                                System.out.print("*");
-                            }
-                        } else if (i == 3 && j == 3) {
-                            System.out.print("    *    ");
-                            for (int k = 0; k < 4; k++) {
-                                System.out.print("*");
-                            }
-                        }else if (i>3 && j==3){
-                                System.out.print("         ****");
-
-                        }
-Thread.sleep(200);
-
-                    }
-                    System.out.println();
-                }
-
-
-    }
 
 }
 
--- src/DailyExercise/Empty2.java
+++ src/DailyExercise/Empty2.java
@@ -2,62 +2,9 @@ package DailyExercise;
 
 public class Empty2 {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        location(10);
-
-
-
-    }
-
-    public static void location(int a) throws InterruptedException {
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 22; j++) {
-                      if (i == 0 && j >= 7  && j <= 15){
-                    System.out.print("*");
-                }else if (i == 1 && j >= 4  && j <= 17){
-                    System.out.print("*");
-                }else if (i == 2 && j >= 2  && j <= 19){
-                    System.out.print("*");
-                }else if (i == 3 && j >= 1  && j <= 20){
-                    System.out.print("*");
-                }else if (i == 4 && j >= 0  ){
-                    System.out.print("*");
-                }else if (i == 5 && j >= 1  && j <= 20){
-                    System.out.print("*");
-                }else if (i == 6 && j >= 2  && j <= 19){
-                    System.out.print("*");
-                }else if (i == 7 && j >= 4  && j <= 17){
-                    System.out.print("*");
-                }else if (i == 8 && j >= 7  && j <= 15){
-                    System.out.print("*");
-                }else {
-                    System.out.print(" ");
-                }
-
-            }
-            System.out.println();
-
-        }
-
-
-    }
-
 
 }
 
-/*
 
-       *   *   *
-    *            *
-  *                *
- *                  *
-*                    *
- *                  *
-  *                *
-    *            *
-      *   *   *
- */
 
 
