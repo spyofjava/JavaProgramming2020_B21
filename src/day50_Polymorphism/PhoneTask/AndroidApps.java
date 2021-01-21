bbd177292b13dc210fccd075aa1a085d7666ef99
Ibrahim <spyofjava@gmail.com>
2021-01-21 09:44:50 -0800

    Jan_21

--- src/day50_Polymorphism/PhoneTask/AndroidApps.java
+++ src/day50_Polymorphism/PhoneTask/AndroidApps.java
@@ -1,14 +1,6 @@
 package day50_Polymorphism.PhoneTask;
 
-public interface AndroidApps {
-
-    String AppStoreName="Play Market";
-    String OS="Android";
-
-
-
+public interface AndroidApps extends Downloadable{
+    String AppStoreName = "Play Market";
+    String OS = "Android";
 }
-/*
-2. create an interface named AndroidApp that can inherit from Downloadable
-            variable: AppStoreName
- */
--- src/day50_Polymorphism/PhoneTask/AppleApps.java
+++ src/day50_Polymorphism/PhoneTask/AppleApps.java
@@ -1,12 +1,7 @@
 package day50_Polymorphism.PhoneTask;
 
-public interface AppleApps extends Downloadable {
-
-    String AppStoreName="Apple Store";
-    String OS="IOS";
+public interface AppleApps  extends Downloadable{
+    String AppStoreName = "Apple";
+    String OS = "iOS 14";
 
 }
-/*
-3. create an interface named AppleApp that can inherit from Downloadable
-            variable: AppStoreName
- */
