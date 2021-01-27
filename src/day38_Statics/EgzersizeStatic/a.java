0f3084b9e57ea35e34231e90038eb53b4745e6d0
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-01-27 09:18:44 -0800

    java27

---
+++ src/day38_Statics/EgzersizeStatic/a.java
@@ -0,0 +1,13 @@
+package day38_Statics.EgzersizeStatic;
+
+import java.util.Scanner;
+
+public class a {
+
+    public static void main(String[] args) {
+
+        Scanner a=new Scanner(System.in);
+        char res=a.next().charAt(0);
+        System.out.println(res);
+    }
+}
