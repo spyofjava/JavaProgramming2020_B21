7c1badfa83784d35364128a1332a7512fa497b50
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-01-27 21:42:39 -0800

    java28

---
+++ src/Office_Hours/Practice_01_27_2021/Main.java
@@ -0,0 +1,10 @@
+package Office_Hours.Practice_01_27_2021;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Fibonacci.howmanyTerm(4));
+
+    }
+}
