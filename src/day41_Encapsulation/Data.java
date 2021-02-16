d598102452f9274a8182149558740fe0dfca1026
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-02-15 20:51:32 -0800

    Feb15

--- src/day41_Encapsulation/Data.java
+++ src/day41_Encapsulation/Data.java
@@ -2,11 +2,11 @@ package day41_Encapsulation;
 
 public class Data {
 
-    public static String publicVariable = "A";
-    private static String privateVariable = "B";
-    static String defaultVariable = "C";  // default access modifier
+    public static String publicVariable = "A";//----line 1
+    private static String privateVariable = "B";//----line 2
+    static String defaultVariable = "C";  // default access modifier //----line 3
 
-    // protected static String protectedVariable = "D";
+     protected static String protectedVariable = "D";//----line 4
 
     Data(){
 
@@ -15,17 +15,20 @@ public class Data {
 
     public static void publicMethod(){
         System.out.println("Public Method");
-    }
+    } //----line 5
 
     private static void privateMethod(){
         System.out.println("private Method");
-    }
+    }//----line 6
 
     static void defaultMethod(){
         System.out.println("default Method");
-    }
+    }//----line 7
 
 
+    public String getprivateVariable(){
+        return privateVariable;
+    }  //----line 8
 
 
 }
--- src/day41_Encapsulation/Test.java
+++ src/day41_Encapsulation/Test.java
@@ -13,6 +13,28 @@ public class Test {
       //  Data.privateMethod();
         Data.defaultMethod();
 
+
+
+
+
+
+
+        //----accessing --->public static
+        System.out.println(Data.publicVariable);
+
+
+        //----accessing --->protected static
+        System.out.println(Data.protectedVariable);
+
+        //----accessing --->default static
+        System.out.println(Data.defaultVariable);
+
+        //----accessing --->orivate static
+       // System.out.println(Data.privateVariable);---> this give us compile time error . we have to use getter setter method to access private variable
+
+        System.out.println(obj.getprivateVariable());  // line 8
+
+
     }
 
 }
